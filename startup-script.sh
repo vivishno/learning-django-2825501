6fbf16ecd7e715520b8b9bad39c8937f36524a68
Sai Praveen <saipraveen-a@github.com>
2020-09-18 10:49:09 +0530

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,5 @@
-python3 manage.py makemigrations
+python3 wisdompets/manage.py makemigrations
 
-python3 manage.py migrate
+python3 wisdompets/manage.py migrate
 
-python3 manage.py runserver
+python3 wisdompets/manage.py runserver
